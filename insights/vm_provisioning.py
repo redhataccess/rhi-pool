ef7bca5d475ed14286cd73726bbc784318daa471
Anisha Narang <anarang@redhat.com>
2016-04-18 17:14:23 +0530

    registering client to RHI

--- insights/vm_provisioning.py
+++ insights/vm_provisioning.py
@@ -1,5 +1,6 @@
 """ Module for provisioning VMs on openstack for testing
 """
+
 import novaclient
 import time
 from novaclient import client
@@ -9,7 +10,7 @@ import sys
 import subprocess
 
 
-class VMError():
+class VMError(Exception):
     """
     Raise errors for the Virtual machine
 
@@ -17,30 +18,33 @@ class VMError():
 
 class VirtualMachine():
 
+    def __init__(self):
+        self.config=configparser.ConfigParser()
+        self.config.read("pool.conf")
+        self.rhn_username = self.config.get('rhn_login', 'rhn_username')
+        self.rhn_password = self.config.get('rhn_login', 'rhn_password')
+
     def get_openstack_client_instance(self):
         """
         Creates client object instance using openstack novaclient API
         :return: openstack client object
 
         """
-        config=configparser.ConfigParser()
-        config.read("pool.conf")
-        username = config.get('openstack_vms','username')
-        api_key = config.get('openstack_vms', 'api_key')
-        auth_url=config.get('openstack_vms', 'auth_url')
-        project_id=config.get('openstack_vms', 'project_id')
+        username = self.config.get('openstack_vms','username')
+        api_key = self.config.get('openstack_vms', 'api_key')
+        auth_url = self.config.get('openstack_vms', 'auth_url')
+        project_id = self.config.get('openstack_vms', 'project_id')
         with client.Client(
             version=2, username=username,
             api_key=api_key, auth_url=auth_url, project_id=project_id
         ) as openstack_client:
             openstack_client.authenticate()
-            print openstack_client
             return openstack_client
 
-    def create_openstack_instance(self, instance_name, image_name, flavor_name, key_name, pool_name, timeout=5 ):
+    def create_openstack_instance(self, instance_name, image_name, flavor_name,
+                                  key_name, pool_name, timeout=5 ):
         self.os_client = self.get_openstack_client_instance()
-        print self.os_client.servers.list()
-        print self.os_client.networks.list()
+        print "List of existing servers: {0}".format(self.os_client.servers.list())
         image = self.os_client.images.find(name=image_name)
         flavor = self.os_client.flavors.find(name=flavor_name)
         self.floating_ip = self.os_client.floating_ips.create(pool=pool_name)
@@ -48,9 +52,10 @@ class VirtualMachine():
 
         # Create openstack instance from given parameters
         print 'Creating openstack instance {0}'.format(instance_name)
-
         print self.os_client
-        self.instance = self.os_client.servers.create(name=instance_name, flavor=flavor, image=image, key_name=key_name)
+        self.instance = self.os_client.servers.create(name=instance_name,
+                                                      flavor=flavor, image=image,
+                                                      key_name=key_name)
         # waiting for the instance to boot
         status = self.instance.status
         print "The VM is booting, status: {0}".format(status)
@@ -70,7 +75,6 @@ class VirtualMachine():
                 print('The timeout for assigning the floating IP has reached!')
                 sys.exit(1)
             try:
-                print time.time()
                 self.instance.add_floating_ip(self.floating_ip)
                 print('SUCCESS!! The floating IP {0} has been assigned '
                       'to instance!'.format(self.floating_ip.ip))
@@ -78,7 +82,7 @@ class VirtualMachine():
             except novaclient.exceptions.BadRequest:
                 time.sleep(5)
 
-        print self.os_client.servers.find(name=instance_name).addresses
+        print "Addresses alloted to VM: {0}".format(self.os_client.servers.find(name=instance_name).addresses)
         print "Trying to ping the instance"
         print self.instance.id
         self.ping(self.floating_ip.ip, instance_name)
@@ -102,7 +106,7 @@ class VirtualMachine():
             output = command.communicate()[0]
             print output
 
-            # Checking if the ping to the openstack instance is successful
+            # Checking if we can ping openstack instance
             if int(output.split()[-1]) == 0:
                 print(
                     'SUCCESS !! The given host {0} has been pinged!!'.format(instance_name))
@@ -115,6 +119,97 @@ class VirtualMachine():
             else:
                 time.sleep(5)
 
+    def rhsm_register(self):
+        """
+        Register VM to subscription manager
+        :return:
+        """
+        ip = self.floating_ip.ip
+        self.ssh_client = paramiko.SSHClient()
+        print self.ssh_client
+        self.ssh_client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+        while True:
+            try:
+                self.ssh_client.connect(hostname=ip, username="cloud-user")
+                print "SSH connection established"
+                break
+            except paramiko.ssh_exception.NoValidConnectionsError:
+                time.sleep(5)
+
+        stdin, stdout, stderr = self.ssh_client.exec_command("hostname")
+        self.hostname = stdout.read()
+        print 'hostname: {0}'.format(self.hostname)
+
+        cmd = 'sudo subscription-manager register --username={0} --password={1}'.format(
+            self.rhn_username, self.rhn_password)
+
+        stdin, stdout, stderr = self.ssh_client.exec_command(cmd, get_pty=True)
+        print stdout.channel.recv_exit_status()
+        print stdout.read(), stderr.read()
+        print "RHSM registration done"
+
+        cmd = 'sudo subscription-manager attach --auto'
+        stdin, stdout, stderr = self.ssh_client.exec_command(cmd, get_pty=True)
+        print stdout.read()
+        status = stdout.channel.recv_exit_status()
+        if status == 0:
+            print "Auto subscription attached"
+        else:
+            raise VMError("Unable to attach auto subscription")
+
+    def register_to_insights(self):
+        """
+        Register client to Subscription Manager
+        Register the client to Red Hat Insights service
+        :return:
+        """
+        self.rhsm_register()
+        stdin,stdout,stderr = self.ssh_client.exec_command("sudo yum install -y redhat-access-insights", get_pty=True)
+        print stdout.read()
+        if stdout.channel.recv_exit_status() != 0:
+            raise VMError("Unable to install redhat-access-insights rpm")
+
+        stdin,stdout,stderr = self.ssh_client.exec_command("rpm -qi redhat-access-insights")
+        print stdout.read()
+
+        stdin, stdout, stderr = self.ssh_client.exec_command("sudo redhat-access-insights --register", get_pty=True)
+        print stdout.read()
+        if stdout.channel.recv_exit_status() != 0:
+            print stderr.read()
+            raise VMError("Registration to RHI not successful")
+
+        stdin, stdout, stderr =  self.ssh_client.exec_command("sudo redhat-access-insights", get_pty=True)
+        print stdout.read()
+        if stdout.channel.recv_exit_status() !=0:
+            raise VMError("Upload not successful")
+
+        stdin, stdout, stderr = self.ssh_client.exec_command("cat /etc/redhat-access-insights/machine-id")
+        self.machine_id = stdout.read()
+        print self.machine_id
+        return self.machine_id
+
+    def unregister_from_rhi(self):
+        """
+        Unregister machine from Red Hat Insights service
+        :return:
+        """
+        stdin, stdout, stderr =  self.ssh_client.exec_command("sudo redhat-access-insights --unregister", get_pty=True)
+        print stdout.read()
+        print stdout.channel.recv_exit_status()
+        if stdout.channel.recv_exit_status() !=0:
+            raise VMError("Unable to unregister the VM from RHI service")
+
+    def unregister_from_RHSM(self):
+        """
+        Unregister machine from Subscription manager
+        :return:
+        """
+        stdin, stdout, stderr = self.ssh_client.exec_command("sudo subscription-manager unregister", get_pty=True)
+        print stdout.read()
+        print stdout.channel.recv_exit_status()
+        if stdout.channel.recv_exit_status() != 0:
+            raise VMError("Unable to subscription-manager unregister")
+
     def delete_openstack_instance(self, instance_name):
         """
         deleting the openstack instance created above
@@ -125,9 +220,3 @@ class VirtualMachine():
         print 'deleting instance: {0}'.format(instance_name)
         print self.instance.id
         self.instance.delete()
-
-
-
-
-
-
