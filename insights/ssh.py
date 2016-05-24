f94971cd6c6b10a84c763d893de388d4422f8902
Anisha Narang <anarang@redhat.com>
2016-05-24 20:05:58 +0530

    adding ssh.py for making ssh connections

---
+++ insights/ssh.py
@@ -0,0 +1,24 @@
+import paramiko
+import logging
+from insights.config import Settings
+
+logger = logging.getLogger(__name__)
+
+class SSHConnection(Settings):
+
+    def get_ssh_connection(self,ip):
+        ssh_client = paramiko.SSHClient()
+        logger.info("initiated paramiko client")
+        ssh_client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+        key_filename = self.get('ssh','key_path')
+
+        while True:
+            try:
+                ssh_client.connect(hostname=ip, username="cloud-user",
+                                        key_filename = key_filename)
+                logger.debug("SSH connection established")
+                break
+            except paramiko.ssh_exception.NoValidConnectionsError:
+                time.sleep(5)
+
+        return ssh_client
