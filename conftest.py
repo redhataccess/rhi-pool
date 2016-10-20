7b04306e3b8f188beaf46550d422d10ec0b3a1b3
Rohit Kadam <rohit.d.kadam@gmail.com>
2016-10-20 17:11:56 +0530

    Updated conftest.py to have full classname in report

--- conftest.py
+++ conftest.py
@@ -2,4 +2,4 @@ import pytest
 
 def pytest_itemcollected(item):
     """Set tests docstring into pytest report instead of class::test_name"""
-    if item._obj.__doc__: item._nodeid = item.obj.__doc__.strip()
+    if item._obj.__doc__: item._nodeid = item.nodeid + " : " + item.obj.__doc__.strip()
