93b6e757f45e21ec4eaf9818c9ebc52c0ae6d2f8
kanchan04katare <kanchan04katare@gmail.com>
2016-10-24 18:57:15 +0530

    Added test for checking rule_ id

--- insights/archive_functions.py
+++ insights/archive_functions.py
@@ -3,10 +3,13 @@ import shutil
 import tarfile
 
 
-def get_system_id(machine_id_path, archive_location):
+def get_system_id(archive_location):
     """ Checking system id from archive """
     tar = tarfile.open(archive_location, 'r')
     tar.extractall()
+    names = tar.getnames()
+    matches = [s for s in names if "machine-id" in str(s)]
+    machine_id_path = matches[0]
     machine_id = tar.extractfile(machine_id_path)
     system_id = machine_id.read()
     tar.close()
@@ -15,6 +18,7 @@ def get_system_id(machine_id_path, archive_location):
 def replace_bash_version(search_file_path, un_archive_file_location, bash_version, new_bash_version):
     """ For introducing rule, replacing bash version with new bashclea
     version in folder and making modified archive with updated bash version  """
+
     with open(search_file_path, "r") as search_file, open("tmp.txt", "w")as new_file:
         for line in search_file:
             if "bash" in line:
