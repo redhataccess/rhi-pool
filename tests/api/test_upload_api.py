a4fbe4a0d57d804ffe9f6bcb4df7488596942670
kanchan04katare <kanchan04katare@gmail.com>
2016-11-07 15:14:00 +0530

    Added Upload api test

--- tests/api/test_upload_api.py
+++ tests/api/test_upload_api.py
@@ -1,7 +1,4 @@
-import json
 import logging
-import os
-import shutil
 import unittest
 import requests
 from insights import archive_functions
@@ -17,45 +14,13 @@ class UploadAPI(unittest.TestCase):
         self.session.cert = self.setting.get_certs()
         self.session.verify = False
         self.base_url = self.setting.get('api', 'url')
-        self.new_bash_version = self.setting.get('upload_archive', 'new_bash_version')
-        self.un_archive_file_location = self.setting.get('upload_archive', 'un_archive_file_location')
         self.archive_location = self.setting.get('upload_archive', 'archive_file_path')
 
     def test_archive_upload(self):
         """ Testing api upload endpoint """
         self.system_id = archive_functions.get_system_id(self.archive_location)
         files = {'file': open(self.archive_location, "rb")}
-        print self.system_id
         self.upload = self.session.post(self.base_url + '/v1/uploads/' + self.system_id,
                                         files=files)
         assert self.upload.status_code == 201
         logging.info("Upload done successfully")
-
-    def test_upload_new_archive(self):
-        """ Uploading new modified archive and checking if rule is detected """
-        self.system_id = archive_functions.get_system_id(self.archive_location)
-
-        archive_functions.replace_bash_version(self.un_archive_file_location,
-                                               self.new_bash_version, self.archive_location)
-        new_archive_file = {'file': open('tarfile_add.tar.gz', "rb")}
-        upload_archive = self.session.post(self.base_url + '/v1/uploads/' + self.system_id,
-                                           files=new_archive_file)
-        assert upload_archive.status_code == 201
-
-        response = json.loads(upload_archive.text)
-        test_rule = 'bash_injection|VULNERABLE_BASH_DETECTED'
-        reports = response['reports']
-        matching_reports = [report for report in reports if report['rule_id'] == test_rule]
-        logging.info(matching_reports)
-        assert len(matching_reports) == 1
-        """Checking bash version"""
-        bash_version = matching_reports[0]
-        assert bash_version['details']['package'] == self.new_bash_version
-        logging.info("archive uploaded successfully, bash vulnerability detected")
-
-    def tearDown(self):
-        """ Deleting archive """
-        if os.path.exists('tarfile_add.tar.gz'):
-            os.remove('tarfile_add.tar.gz')
-        shutil.rmtree(self.un_archive_file_location)
-        logging.info('successfully deleted tarfile')
